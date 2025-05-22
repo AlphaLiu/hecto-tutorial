1c84929530a1149c42768a7f71b2069b58d301f3
Alpha Liu <alpha.liu@me.com>
2025-05-22 15:01:36 +0800

    Expiring Messages

--- src/editor/messagebar.rs
+++ src/editor/messagebar.rs
@@ -1,34 +1,69 @@
-use std::io::Error;
+use std::{
+    io::Error,
+    time::{Duration, Instant},
+};
 
 use super::{
     terminal::{Size, Terminal},
     uicomponent::UIComponent,
 };
 
+const DEFAULT_DURATION: Duration = Duration::new(5, 0);
+
+struct Message {
+    text: String,
+    time: Instant,
+}
+impl Default for Message {
+    fn default() -> Self {
+        Self {
+            text: String::new(),
+            time: Instant::now(),
+        }
+    }
+}
+impl Message {
+    fn is_expired(&self) -> bool {
+        Instant::now().duration_since(self.time) > DEFAULT_DURATION
+    }
+}
+
 #[derive(Default)]
 pub struct MessageBar {
-    current_message: String,
+    current_message: Message,
     needs_redraw: bool,
+    clear_after_expiry: bool,
 }
 
 impl MessageBar {
     pub fn update_message(&mut self, new_message: String) {
-        if new_message != self.current_message {
-            self.current_message = new_message;
-            self.needs_redraw = true;
-        }
+        self.current_message = Message {
+            text: new_message,
+            time: Instant::now(),
+        };
+        self.clear_after_expiry = true;
+        self.set_needs_redraw(true);
     }
 }
 
 impl UIComponent for MessageBar {
-    fn mark_redraw(&mut self, value: bool) {
+    fn set_needs_redraw(&mut self, value: bool) {
         self.needs_redraw = value;
     }
     fn needs_redraw(&self) -> bool {
-        self.needs_redraw
+        (!self.clear_after_expiry && self.current_message.is_expired()) || self.needs_redraw
     }
     fn set_size(&mut self, _: Size) {}
     fn draw(&mut self, origin: usize) -> Result<(), Error> {
-        Terminal::print_row(origin, &self.current_message)
+        if self.current_message.is_expired() {
+            self.clear_after_expiry = true; // Upon expiration, we need to write out "" once to clear the message. To avoid clearing more than necessary, we keep track of the fact that we've already clared the expired message once.
+        }
+        let message = if self.current_message.is_expired() {
+            ""
+        } else {
+            &self.current_message.text
+        };
+
+        Terminal::print_row(origin, message)
     }
 }
